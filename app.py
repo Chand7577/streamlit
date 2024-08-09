1d80fe412373e8284e621c1f5c9f0af97949e1ae
Amrit <100637966+Chand7577@users.noreply.github.com>
2024-08-09 03:55:19 +0000

    Update app.py
    bot's ready

--- app.py
+++ app.py
@@ -1,27 +1,40 @@
-import  streamlit as st
+import streamlit as st
+from openai import OpenAI
 
-st.title('Namaste Sahil beteh,neeche kuch type kar')
-base="blue"
+st.title('BOT LIKE CHATGPT(Detroit)')
+
+client = OpenAI( 
+    api_key="25ed35d2f1a94c5290e4004c4366a403",
+    base_url="https://api.aimlapi.com",)
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "mistralai/Mistral-7B-Instruct-v0.2"
+
+
 if "history" not in st.session_state:
-    st.session_state.history=[]
-
+    st.session_state.history = []
 
 for message in st.session_state.history:
     with st.chat_message(message["avatar"]):
         st.markdown(message["message"])
 
 
-
-if prompt:=st.chat_input("type something"):
+if prompt := st.chat_input("type something"):
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    st.session_state.history.append({"avatar":"user","message":prompt})
+    st.session_state.history.append({"avatar": "user", "message": prompt})
 
-    response=f"Echo: {prompt}"
     with st.chat_message("assistant"):
-        st.markdown(response)
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["avatar"], "content": m["message"]}
+                for m in st.session_state.history
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
 
-    st.session_state.history.append({"avatar":"assistant","message":prompt})
-
+    st.session_state.history.append({"avatar": "assistant", "message": prompt})
 
